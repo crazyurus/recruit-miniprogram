1d1bda73fd75aad01cf35af6ba0ef55302f0ef8d
廖星 <xing.liao@wutnews.net>
2017-08-28 00:27:34 +0800

    完成宣讲会详情页

--- pages/xjh/detail.js
+++ pages/xjh/detail.js
@@ -2,7 +2,11 @@ var app = getApp();
 Page({
   data: {
     article: {},
-    bgcolor: '45c8dc'
+    bgcolor: '45c8dc',
+    tab: {
+      active: [true, false, false],
+      disabled: [false, true, false]
+    }
   },
   onLoad(options) {
     wx.setNavigationBarColor({
@@ -15,11 +19,62 @@ Page({
 
     app.getApiData('https://api.wutnews.net/recruit/haitou/xjh/view?client=wutnews&id=' + options.id, {}, false).then((result) => {
       result.isUniversityLogo = result.logoUrl.indexOf('/university') > -1;
+      result.content = result.content.replace(/<table border=1 cellspacing=0 cellpadding=0>/g, '<table style="border: 1px solid #c8c7cc">');
 
       this.setData({
         article: result,
-        bgcolor: options.bgcolor
+        bgcolor: options.bgcolor,
+        'tab.disabled': [result.content == '', result.albums.length == 0, result.xjhs.length == 0]
       });
+    });
+  },
+  onShareAppMessage(res) {
+    return {
+      title: this.data.article.company,
+      path: '/pages/xjh/detail?id=' + this.data.article.id + '&bgcolor=' + this.data.bgcolor,
+      success(res) {
+        wx.showToast({
+          title: '分享成功'
+        });
+      },
+      fail(res) {
+        wx.showToast({
+          title: '分享失败'
+        });
+      }
+    }
+  },
+  clickTabContent() {
+    if (this.data.tab.disabled[0]) return;
+
+    this.setData({
+      'tab.active': [true, false, false]
+    });
+  },
+  clickTabAlbum() {
+    if (this.data.tab.disabled[1]) return;
+
+    this.setData({
+      'tab.active': [false, true, false]
+    });
+  },
+  clickTabXjh() {
+    if (this.data.tab.disabled[2]) return;
+
+    this.setData({
+      'tab.active': [false, false, true]
+    });
+  },
+  showImagePreview(e) {
+    let imgArray = [];
+
+    this.data.article.albums.map((item) => {
+      imgArray.push(item.originalUrl);
+    });
+
+    wx.previewImage({
+      current: e.target.dataset.src,
+      urls: imgArray
     });
   }
 });
