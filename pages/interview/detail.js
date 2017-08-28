1d4eddaa742c55b40de7dfae00de76d8f39b123a
廖星 <xing.liao@wutnews.net>
2017-08-28 19:08:26 +0800

    完成面经详情页

---
+++ pages/interview/detail.js
@@ -0,0 +1,34 @@
+var app = getApp();
+Page({
+  data: {
+    article: {}
+  },
+  onLoad(options) {
+    app.getApiData('https://api.wutnews.net/recruit/haitou/mjfx/view?client=m&encrypt=0&id=' + options.id, {}, false).then((result) => {
+      let position = [];
+
+      for (let category of result.workCategorys) {
+        position.push(category.name);
+      }
+
+      result.position = position.join(' ');
+      result.info = result.info.replace(/(&ldquo;|&rdquo)/g, '"').replace(/&times;/g, '×').replace(/(&lsquo;|&rsquo;)/g, "'");
+    
+      this.setData({
+        article: result
+      });
+    });
+  },
+  onShareAppMessage(res) {
+    return {
+      title: this.data.article.title,
+      path: '/pages/interview/detail?id=' + this.data.article.id,
+      imageUrl: this.data.article.image,
+      success(res) {
+        wx.showToast({
+          title: '分享成功'
+        });
+      }
+    }
+  }
+});
