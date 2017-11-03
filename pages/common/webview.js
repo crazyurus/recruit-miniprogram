99c97ccdbf76bd4e695f4d46db936018e11801e2
廖星 <xing.liao@wutnews.net>
2017-11-03 11:18:09 +0800

    添加分享功能

--- pages/common/webview.js
+++ pages/common/webview.js
@@ -7,5 +7,16 @@ Page({
       this.setData({
         url: 'https://web.wutnews.net/recruit/index/wechat?url=' + encodeURIComponent(options.url)
       });
+  },
+  onShareAppMessage(options) {
+    return {
+      title: '武汉理工大学就业招聘',
+      path: '/pages/common/webview?url=' + encodeURIComponent('https://web.wutnews.net/recruit/index/wechat?url=' + encodeURIComponent(options.webViewUrl)),
+      success(res) {
+        wx.showToast({
+          title: '分享成功'
+        });
+      }
+    }
   }
 });
