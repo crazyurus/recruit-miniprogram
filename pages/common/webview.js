42155f8410e026f499e3d86615c6f73312f4b29d
廖星 <xing.liao@wutnews.net>
2018-03-01 13:10:33 +0800

    URL修复

--- pages/common/webview.js
+++ pages/common/webview.js
@@ -5,13 +5,13 @@ Page({
   },
   onLoad(options) {
     this.setData({
-      url: 'https://web.wutnews.net/recruit/index/wechat?url=' + encodeURIComponent(options.url)
+      url: 'https://web.wutnews.net/recruit/wechat/article?url=' + encodeURIComponent(options.url)
     });
   },
   onShareAppMessage(options) {
     return {
       title: '武汉理工大学就业招聘',
-      path: '/pages/common/webview?url=' + encodeURIComponent('https://web.wutnews.net/recruit/index/wechat?url=' + encodeURIComponent(options.webViewUrl)),
+      path: '/pages/common/webview?url=' + encodeURIComponent('https://web.wutnews.net/recruit/wechat/article?url=' + encodeURIComponent(options.webViewUrl)),
       success(res) {
         wx.showToast({
           title: '分享成功'
