42598d0667c529d0f9421a246dcb07be0465de25
廖星 <xing.liao@wutnews.net>
2021-12-09 01:47:01 +0800

    吐槽兼容 QQ

--- pages/me/index.js
+++ pages/me/index.js
@@ -1,16 +1,26 @@
 const app = getApp();
 
 Page({
-  data: {
-    tocaoExtraData: {
-      id: 23796,
-    },
+  data: {},
+  tucao() {
+    if (app.isQQ) {
+      this.openURL('https://support.qq.com/products/23796');
+    } else {
+      wx.navigateToMiniProgram({
+        appId: 'wx8abaf00ee8c3202e',
+        extraData: {
+          id: 23796,
+        },
+        envVersion: 'release',
+      })
+    }
   },
   about() {
-    const url = 'https://mp.weixin.qq.com/s/HbXW7A87ilgW_CbEI-1ODQ';
-
+    this.openURL('https://mp.weixin.qq.com/s/HbXW7A87ilgW_CbEI-1ODQ');
+  },
+  openURL(url) {
     wx.navigateTo({
       url: '/pages/common/webview?url=' + encodeURIComponent(url),
-    })
-  },
+    });
+  }
 })
