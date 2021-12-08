c807c7a840fa81a3d5a831a7d2c4c9b4102ddb24
廖星 <xing.liao@wutnews.net>
2021-12-09 01:03:10 +0800

    完成新的页面我

---
+++ pages/common/webview.js
@@ -0,0 +1,19 @@
+const app = getApp();
+
+Page({
+  data: {
+    url: '',
+  },
+  onLoad(options) {
+    this.setData({
+      url: decodeURIComponent(options.url)
+    });
+  },
+  onShareAppMessage() {
+    return {
+      success() {
+        app.toast('分享成功');
+      }
+    };
+  }
+})
