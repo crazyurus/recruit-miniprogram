165c99b7ed83da771cd76d68291c29d0932a148e
廖星 <xing.liao@wutnews.net>
2021-12-09 02:28:47 +0800

    校园资讯页面

---
+++ pages/me/post/detail.js
@@ -0,0 +1,63 @@
+const app = getApp();
+
+Page({
+  data: {
+    title: false,
+    article: {},
+    contentStyle: {
+      a: 'color: #45c8dc',
+    },
+  },
+  onLoad(options) {
+    if (app.globalData.post) {
+      this.setData({
+        'article.title': app.globalData.post.title,
+      });
+    }
+
+    wx.request({
+      method: 'GET',
+      url: 'https://test-api-iwut.itoken.team/v1/news/content?postId=' + options.id,
+      success: result => {
+        const article = result.data.data;
+
+        this.setData({
+          'article.content':  article.content.replace(/font/g, 'f').replace('<div class="art_tit">', '<div class="art_tit" style="display: none">').replace('<title>新闻详情</title>', ''),
+        });
+      },
+    });
+  },
+  onUnload() {
+    app.globalData.post = null;
+  },
+  onPageScroll(e) {
+    if (e.scrollTop <= 200 && this.data.title) {
+      this.data.title = false;
+      wx.setNavigationBarTitle({
+        title: ' '
+      });
+    }
+    if (e.scrollTop > 200 && !this.data.title) {
+      this.data.title = true;
+      wx.setNavigationBarTitle({
+        title: this.data.article.title
+      });
+    }
+  },
+  onShareAppMessage() {
+    return {
+      title: this.data.article.title,
+      path: app.sharePath(this),
+      success() {
+        wx.showToast({
+          title: '分享成功',
+        });
+      }
+    };
+  },
+  onShareTimeline() {
+    return {
+      title: this.data.article.title,
+    };
+  },
+});
