95c0ff7534887508e7afb18ae2112bcfb1c783a6
廖星 <xing.liao@wutnews.net>
2021-12-09 01:17:14 +0800

    页面我的大多数功能

--- pages/me/index.js
+++ pages/me/index.js
@@ -2,58 +2,12 @@ const app = getApp();
 
 Page({
   data: {
-    list: [
-      {
-        name: '就业指导',
-        children: [
-          {
-            name: '求职攻略',
-            url: app.isQQ ? '' : 'https://mp.weixin.qq.com/mp/homepage?__biz=MjM5MjgwMDkzNQ==&hid=13&sn=bb51034acd78f19ae7a5e67ffe34ebab&scene=18',
-          },
-          {
-            name: '职业咨询',
-            url: 'https://mp.weixin.qq.com/mp/homepage?__biz=MjM5MjgwMDkzNQ==&hid=13&sn=bb51034acd78f19ae7a5e67ffe34ebab&scene=18',
-          }
-        ]
-      },
-      {
-        name: '就业资讯',
-        children: [
-          {
-            name: '军队文职招聘',
-            url: app.isQQ ? '' : 'https://mp.weixin.qq.com/mp/homepage?__biz=MjM5MjgwMDkzNQ==&hid=13&sn=bb51034acd78f19ae7a5e67ffe34ebab&scene=18',
-          },
-          {
-            name: '国际组织实习',
-            url: 'https://mp.weixin.qq.com/mp/homepage?__biz=MjM5MjgwMDkzNQ==&hid=13&sn=bb51034acd78f19ae7a5e67ffe34ebab&scene=18',
-          },
-          {
-            name: '就业服务清单',
-            url: 'https://mp.weixin.qq.com/mp/homepage?__biz=MjM5MjgwMDkzNQ==&hid=13&sn=bb51034acd78f19ae7a5e67ffe34ebab&scene=18',
-          },
-          {
-            name: '企业介绍',
-            url: 'https://mp.weixin.qq.com/mp/homepage?__biz=MjM5MjgwMDkzNQ==&hid=13&sn=bb51034acd78f19ae7a5e67ffe34ebab&scene=18',
-          }
-        ]
-      },
-      {
-        name: '企业服务',
-        children: [
-          {
-            name: '毕业生生源',
-            url: 'https://mp.weixin.qq.com/s/iTGigDYwZAAcC3cLwl4fDw',
-          },
-          {
-            name: '帮推须知',
-            url: 'https://mp.weixin.qq.com/s/sWw23DzbA_jbIrxhHwlbRw',
-          }
-        ]
-      },
-    ]
+    tocaoExtraData: {
+      id: 23796,
+    },
   },
-  openUrl(e) {
-    const { url } = e.currentTarget.dataset;
+  about() {
+    const url = 'https://mp.weixin.qq.com/s/HbXW7A87ilgW_CbEI-1ODQ';
 
     wx.navigateTo({
       url: '/pages/common/webview?url=' + encodeURIComponent(url),
