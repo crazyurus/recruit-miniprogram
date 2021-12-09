d378f5b982d575e93a8a1ac3d7e20e6977e66b8d
廖星 <crazyurus@vip.qq.com>
2021-12-09 16:10:10 +0800

    增加隐私协议页面

--- pages/me/index.js
+++ pages/me/index.js
@@ -31,7 +31,10 @@ Page({
     }
   },
   privacy() {
-    this.openURL('https://mp.weixin.qq.com/wxawap/waprivacyinfo?action=show&appid=wx21545de6d74f4b0b&uin=MjE3MjA3MzcyMQ%3D%3D&key=58032826474225cbdd777f132abf3616d0431c9e8ded5daffea7ac9a2c53a6dd3cd690621dc75d14fba5de56f17c3071a21cc04ab19e2a8e84c14f8a9d72f4891b1efdedc61142d2564a4915b7102538c32d0174f069bd6efc250ed12c10be4be5fd0a4df352050c2dc1e3985fdd70fffbac723d08174cd0b5eafe96d38a4ca9');
+    wx.navigateToMiniProgram({
+      appId: this.data.isQQ ? '1108338344' : 'wxd45c635d754dbf59',
+      path: 'pages/detail/detail?url=' + encodeURIComponent('https://docs.qq.com/doc/DRVRrVFZpZllsWU9k'),
+    });
   },
   about() {
     this.openURL('https://mp.weixin.qq.com/s/HbXW7A87ilgW_CbEI-1ODQ');
