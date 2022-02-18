84fcc731bbb3dee3853ee18cadd82142c8e47918
Crazy Urus <crazyurus@vip.qq.com>
2022-02-18 18:30:59 +0800

    refactor: 请求库更换为 axios

--- library/request.js
+++ library/request.js
@@ -1,32 +1,52 @@
+const axios = require('axios');
+const adapter = require('axios-miniprogram-adapter');
 const { toast } = require('./ui');
 const logger = require('./logger');
 
+axios.defaults.adapter = adapter;
+
+const instance = axios.create({
+  baseURL: 'https://a.jiuyeb.cn/mobile.php',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    Auth: 'Baisc MTAyNDY6MTAyNDY=',
+  },
+});
+
+instance.interceptors.request.use(config => {
+  Object.assign(config.data, {
+    school_id: 'b525083d-b83c-4c7e-892f-29909421d961',
+    login_user_id: 1,
+    login_admin_school_code: '',
+    login_admin_school_id: 'b525083d-b83c-4c7e-892f-29909421d961',
+  });
+
+  return config;
+});
+
+instance.interceptors.response.use(response => {
+  if (response.status !== 200) {
+    return Promise.reject('服务器错误 ' + result.status);
+  }
+
+  if (response.data.code === 0) {
+    return response.data.data;
+  }
+
+  return Promise.reject(response.data.msg);
+}, error => {
+  return Promise.reject(error);
+});
+
 function request(url, data = {}, loading = true) {
   if (loading) wx.showNavigationBarLoading();
-  return wx.promises.request({
-    url: 'https://a.jiuyeb.cn/mobile.php' + url,
-    method: 'POST',
-    dataType: 'json',
-    header: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      Auth: 'Baisc MTAyNDY6MTAyNDY=',
-    },
-    data: {
-      school_id: 'b525083d-b83c-4c7e-892f-29909421d961',
-      login_user_id: 1,
-      login_admin_school_code: '',
-      login_admin_school_id: 'b525083d-b83c-4c7e-892f-29909421d961',
-      ...data,
-    }
-  }).then(result => {
-    if (result.statusCode !== 200) return Promise.reject('服务器错误 ' + result.statusCode);
-    else if (result.data.code === 0) return result.data.data;
-    else Promise.reject(result.msg);
-  }, result => {
-    return Promise.reject(result.errMsg);
+  return instance({
+    url,
+    data,
   }).catch(error => {
-    logger.error('[Request] scc', url, error);
-    toast('网络错误');
+    logger.error('[Request] scc', url, error.message);
+    toast(error.message);
   }).finally(() => {
     if (loading) wx.hideNavigationBarLoading();
   });
