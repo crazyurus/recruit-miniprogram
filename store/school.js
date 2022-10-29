42f458a742bdfb9fb0fedd5fecaa7586283250e7
Crazy Urus <crazyurus@vip.qq.com>
2022-10-29 23:57:53 +0800

    feat: 学校持久化存储

--- store/school.js
+++ store/school.js
@@ -1,9 +1,24 @@
 const schools = require('../data/school');
 const SET_SCHOOL = 'SET_SCHOOL';
 
-function reducer(state = schools[0], action) {
+function getDefaultSchool() {
+  const school = wx.getStorageSync('school');
+
+  if (school) {
+    return school;
+  }
+
+  return schools[0];
+}
+
+function reducer(state = getDefaultSchool(), action) {
   switch (action.type) {
     case SET_SCHOOL:
+      wx.setStorage({
+        key: 'school',
+        data: action.payload,
+      });
+
       return action.payload;
     default:
       return state;
